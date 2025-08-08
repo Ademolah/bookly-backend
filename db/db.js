3bad146e3073f1b9b3b564604d54ec005f543e1a
Charles <adeakinyemi129@gmail.com>
2025-08-08 17:57:54 +0100

    updated mongoconn

--- db/db.js
+++ db/db.js
@@ -1,11 +1,23 @@
 const mongoose = require('mongoose')
 
+// async function connectDB() {
+//     try {
+//         await mongoose.connect(process.env.MONGO_URI, {
+//         useNewUrlParser: true,
+//         useUnifiedTopology: true
+//         }).then(()=>console.log('connected to db successfully')
+//         ).catch((error)=>console.log(`Something went wrong ${error}`)
+//         )
+//     } catch (error) {
+//         console.log(`Something went wrong ${error}`);
+        
+//     }
+// }
+
 async function connectDB() {
     try {
-        await mongoose.connect(process.env.MONGO_URI, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true
-        }).then(()=>console.log('connected to db successfully')
+        await mongoose.connect(process.env.MONGO_URI
+        ).then(()=>console.log('connected to db successfully')
         ).catch((error)=>console.log(`Something went wrong ${error}`)
         )
     } catch (error) {
